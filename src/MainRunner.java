347c61ef0b502883b344a6bbb45f2e6258b9ec71
BT_1E10_21 <sample@bths.edu>
2022-03-10 12:31:56 -0500

    Implemented real 2d array encrypt

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainRunner {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        /*Scanner scan = new Scanner(System.in);
 
         int row = scan.nextInt();
         int col = scan.nextInt();
@@ -16,5 +17,13 @@ public class MainRunner {
         //System.out.println(encryptor.encryptMessage(crpyt));
 
         scan.close();
+
+         */
+
+        TwoDArrayEncryptor test = new TwoDArrayEncryptor("123456789123",2,3,1);
+        System.out.println(test);
+        test.encrypt();
+        System.out.println(test);
+
     }
 }
